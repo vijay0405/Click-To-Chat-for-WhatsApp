a13f426e12dcc96bf6c9d1da3619960f4b91dcc8
vijay0405 <uvijaysimha@gmail.com>
2020-06-02 14:00:50 +0530

    added fonts and styling

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'WhatsNum',
-      theme: new ThemeData(
-        primarySwatch: Colors.red,
-      ),
       home: new MyHomePage(),
     );
   }
@@ -25,6 +22,21 @@ class MyHomePage extends StatelessWidget {
     final deviceSize = MediaQuery.of(context).size;
     return MaterialApp(
       title: 'WhatsNum',
+            theme: ThemeData(
+        primarySwatch:Colors.teal,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1 : TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          body2 : TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold
+          )
+        )
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('WhatsApp Number'),
