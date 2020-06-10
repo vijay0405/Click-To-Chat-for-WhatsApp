c7abaa7491b800800f486ea1134a9dbd718f7d18
vijay0405 <uvijaysimha@gmail.com>
2020-06-10 23:49:02 +0530

    added lottie animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:country_code_picker/country_code_picker.dart';
 void main() => runApp(MyApp());
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -16,27 +16,23 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return MaterialApp(
       title: 'WhatsNum',
-            theme: ThemeData(
-        primarySwatch:Colors.teal,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          body1 : TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-          body2 : TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-          title: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-            fontWeight: FontWeight.bold
-          )
-        )
-      ),
+      theme: ThemeData(
+          primarySwatch: Colors.teal,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
       home: Scaffold(
         appBar: AppBar(
           title: Text('WhatsApp Number'),
@@ -56,6 +52,7 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
+            Lottie.asset('assets/paper-plane.json'),
             SingleChildScrollView(
               child: Container(
                 height: deviceSize.height,
@@ -71,7 +68,7 @@ class MyHomePage extends StatelessWidget {
                   ],
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
@@ -87,6 +84,7 @@ class MessageCard extends StatefulWidget {
 class _MessageCardState extends State<MessageCard> {
   final GlobalKey<FormState> _formKey = GlobalKey();
   var phoneNumber = "";
+  var countryCode = "91";
 
   void _launchURL() async {
     if (!_formKey.currentState.validate()) {
@@ -95,12 +93,18 @@ class _MessageCardState extends State<MessageCard> {
     }
     _formKey.currentState.save();
 
-    var url = 'https://api.WhatsApp.com/send?phone=' + phoneNumber;
+    var url =
+        'https://api.WhatsApp.com/send?phone=' + countryCode + phoneNumber;
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       throw 'Could not launch $url';
     }
+  }
+
+  void _changedcountry(ct) {
+    countryCode = ct.toString().substring(1);
+    print(countryCode);
   }
 
   @override
@@ -114,7 +118,7 @@ class _MessageCardState extends State<MessageCard> {
       child: AnimatedContainer(
         duration: Duration(milliseconds: 300),
         curve: Curves.easeIn,
-        height: 260,
+        height: 360,
         constraints: BoxConstraints(minHeight: 260),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
@@ -123,6 +127,18 @@ class _MessageCardState extends State<MessageCard> {
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
+                CountryCodePicker(
+                  onChanged: _changedcountry,
+                  // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                  initialSelection: 'IN',
+                  favorite: ['+91', 'IN'],
+                  // optional. Shows only country name and flag
+                  showCountryOnly: false,
+                  // optional. Shows only country name and flag when popup is closed.
+                  showOnlyCountryWhenClosed: false,
+                  // optional. aligns the flag and the Text left
+                  alignLeft: false,
+                ),
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Enter Phone Number'),
                   keyboardType: TextInputType.number,
@@ -151,7 +167,10 @@ class _MessageCardState extends State<MessageCard> {
                 SizedBox(
                   height: 20,
                 ),
-                Text("The phone number should contain country code, without any symbols like (918099891235)")
+                Text(
+                    "Select the country code from above picker, and enter phone number "),
+                Text("Ex: (8099891235)",
+                    style: TextStyle(fontFamily: "RobotoCondensed"))
               ],
             ),
           ),
