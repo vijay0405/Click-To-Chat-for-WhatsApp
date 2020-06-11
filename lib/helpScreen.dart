eb8cfa5a419d25014cdc6b6898f288e405329a07
vijay0405 <uvijaysimha@gmail.com>
2020-06-12 00:06:38 +0530

    added steps in help screen

--- lib/helpScreen.dart
+++ lib/helpScreen.dart
@@ -11,12 +11,12 @@ class HelpScreen extends StatelessWidget {
         body: SingleChildScrollView(
             child: Column(
           children: <Widget>[
-            Lottie.asset(
-              'assets/character.json',
-              width: 100,
-              height: 150,
-              fit: BoxFit.fill,
-            ),
+            // Lottie.asset(
+            //   'assets/character.json',
+            //   width: 100,
+            //   height: 150,
+            //   fit: BoxFit.fill,
+            // ),
             Padding(
               padding: EdgeInsets.all(10),
               child: Text(
@@ -28,13 +28,25 @@ class HelpScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Image.asset('assets/images/phone-input.jpg'),
+            Image.asset('assets/images/select-country.jpg', cacheHeight: 300, cacheWidth: 250,),
             Padding(
               padding: EdgeInsets.all(10),
               child: Text(
-                "This App will help you in sending messages to new numbers which are not saved in your conacts.",
+                "Choose the country from the picker shown above.",
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 15,
+                  fontFamily: 'Raleway',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Image.asset('assets/images/phone-input.jpg', cacheHeight: 300, cacheWidth: 250,),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Enter the phone number in the input shown in above picture and click on start messaging button.",
+                style: TextStyle(
+                  fontSize: 15,
                   fontFamily: 'Raleway',
                   fontWeight: FontWeight.w500,
                 ),
