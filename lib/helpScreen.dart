e4554a8e59b18848bb8a3f142d9632d557faddb0
vijay0405 <uvijaysimha@gmail.com>
2020-06-11 23:26:37 +0530

    added content

--- lib/helpScreen.dart
+++ lib/helpScreen.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class HelpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Help"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "This App will help you in sending messages to new numbers which are not saved in your conacts.",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Raleway',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
