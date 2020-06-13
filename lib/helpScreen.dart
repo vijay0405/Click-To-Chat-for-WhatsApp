4333d36bc2edeedd197516f2de5d9629214b9a19
vijay0405 <uvijaysimha@gmail.com>
2020-06-13 23:13:18 +0530

    updated help screen ui

--- lib/helpScreen.dart
+++ lib/helpScreen.dart
@@ -22,31 +22,44 @@ class HelpScreen extends StatelessWidget {
               child: Text(
                 "This App will help you in sending messages to new numbers which are not saved in your conacts.",
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 14,
                   fontFamily: 'Raleway',
                   fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-            Image.asset('assets/images/select-country.jpg', cacheHeight: 300, cacheWidth: 250,),
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              child: Image.asset(
+                'assets/images/select-country.jpg',
+                cacheHeight: 300,
+                cacheWidth: 250,
+              ),
+            ),
             Padding(
               padding: EdgeInsets.all(10),
               child: Text(
-                "Choose the country from the picker shown above.",
+                "Choose the country from the picker shown in above picture.",
                 style: TextStyle(
-                  fontSize: 15,
+                  fontSize: 14,
                   fontFamily: 'Raleway',
                   fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-            Image.asset('assets/images/phone-input.jpg', cacheHeight: 300, cacheWidth: 250,),
+            ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                child: Image.asset(
+                  'assets/images/phone-input.jpg',
+                  cacheHeight: 300,
+                  cacheWidth: 250,
+                )),
             Padding(
-              padding: EdgeInsets.all(10),
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 60),
               child: Text(
                 "Enter the phone number in the input shown in above picture and click on start messaging button.",
                 style: TextStyle(
-                  fontSize: 15,
+                  fontSize: 14,
                   fontFamily: 'Raleway',
                   fontWeight: FontWeight.w500,
                 ),
