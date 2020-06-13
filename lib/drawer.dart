a4dba940151461b16d8374f3dbca63dd961f484c
vijay0405 <uvijaysimha@gmail.com>
2020-06-13 23:24:39 +0530

    added version in drawer

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:package_info/package_info.dart';
 import 'helpScreen.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -17,6 +17,15 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String appName = 'watsapp number';
+    String version = "2.0.0";
+
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      appName = packageInfo.appName;
+      String packageName = packageInfo.packageName;
+      version = packageInfo.version;
+      String buildNumber = packageInfo.buildNumber;
+    });
     return Drawer(
         child: Column(
       children: <Widget>[
@@ -35,6 +44,18 @@ class MainDrawer extends StatelessWidget {
         SizedBox(
           height: 20,
         ),
+        Container(
+          height: 120,
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerLeft,
+          color: Colors.teal,
+          child: Text(appName.toString() + version.toString(),
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromRGBO(255, 254, 229, 1),
+              )),
+        ),
         buildListTile("Help", Icons.help, () {
           Navigator.push(
             context,
@@ -43,7 +64,6 @@ class MainDrawer extends StatelessWidget {
             ),
           );
         }),
-
       ],
     ));
   }
