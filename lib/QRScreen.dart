eb0b1c8d82b8b49f788e694864b475f7a6fc0a84
vijay0405 <uvijaysimha@gmail.com>
2020-06-30 23:41:15 +0530

    generate qr on input

--- lib/QRScreen.dart
+++ lib/QRScreen.dart
@@ -7,26 +7,51 @@ class QRScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey();
+    var phoneNumber = "";
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Share QR"),
       ),
       body: SingleChildScrollView(
-        child: Column(children: <Widget>[
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              BarcodeWidget(
-                barcode: Barcode.qrCode(
-                  errorCorrectLevel: BarcodeQRCorrectionLevel.high,
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                BarcodeWidget(
+                  barcode: Barcode.qrCode(
+                    errorCorrectLevel: BarcodeQRCorrectionLevel.high,
+                  ),
+                  data: phoneNumber,
+                  width: 200,
+                  height: 200,
                 ),
-                data: 'https://pub.dev/packages/barcode_widget',
-                width: 200,
-                height: 200,
-              ),
-            ],
+                TextFormField(
+                  decoration:
+                      InputDecoration(labelText: 'Enter Your Phone Number'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Invalid input!';
+                    }
+                  },
+                  onSaved: (value) {
+                    phoneNumber = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                    "Share this geneated qr with any one instead of sharing number."),
+                // Text("Ex: (8099891235)",
+                //     style: TextStyle(fontFamily: "RobotoCondensed"))
+              ],
+            ),
           ),
-        ]),
+        ),
       ),
     );
   }
